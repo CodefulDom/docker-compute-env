976b02d169410bfddfc8b437ec6e3b1b2afbc5bf
codefuldom <dom@codefuldom.com>
2020-01-29 12:15:30 -0600

    :rocket: add app to project

---
+++ app/src/index.js
@@ -0,0 +1,20 @@
+let num = 1001
+console.log(typeof num)
+const newString = String(num)
+console.log(newString.length)
+const str = 'Life, the universe and Everything'
+console.log(`${str} -- ${str.length}`)
+const family = ['little D', 'Big D', 'Mom', 'Steph', 'Silly ass Leo']
+console.log(family)
+console.log(family[2])
+let value = true
+console.log(value)
+
+value = String(value)
+console.log(typeof value)
+
+const doMath = '6' + '2'
+console.log(typeof doMath)
+
+let age = '4'
+console.log(Number(age))
